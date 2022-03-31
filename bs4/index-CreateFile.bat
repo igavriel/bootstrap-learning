1b1204ca47511ce35f265ebf3b42e778d2d58a67
igavriel <igavriel@gmail.com>
2022-03-31 08:53:22 +0300

    Header and small for all

--- bs4/index-CreateFile.bat
+++ bs4/index-CreateFile.bat
@@ -2,6 +2,7 @@
 SET FILE=index.html
 
 echo ^<!DOCTYPE html^>^<html^>^<body^> >%FILE%
+echo ^<h1^>%CD%^</h1^> >>%FILE%
 for %%i in (*.html) do (
    echo ^<li^>^<a href="%%i"^>%%i^</a^>^</li^> >>%FILE%
 )
